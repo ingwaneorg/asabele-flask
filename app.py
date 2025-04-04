714ab0c9be4ecb5528f7fd6ef677c3838c0ed724
barbourians <barbourians@gmail.com>
2025-04-04 21:36:33 +0100

    Tutor and poll done

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
+import os
 import re
 import json
 
 from datetime import datetime
 
-from flask import Flask, request, render_template, abort
+from flask import Flask
+from flask import request
+from flask import render_template, abort, url_for
 
 app = Flask(__name__)
 app.debug = True  # Or set this via config or environment
 
+# Constants
+DB_FILE = 'logs/db.jsonXX'
+
 # Initilise DB
 db = {}
+if app.debug and os.path.exists(DB_FILE):
+    with open(DB_FILE, 'r') as f:
+        db = json.load(f)
 
 
-def save_db(filename='logs/db.json'):
+def save_db(filename=DB_FILE):
     with open(filename, 'w') as f:
         json.dump(db, f, default=str, indent=4)
 
@@ -59,7 +68,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/<room_code>', methods=['GET', 'POST'])
-def learner(room_code):
+def learner_view(room_code):
     template_name = 'learner.html'
     
     # Make sure that the room code is valid
@@ -84,6 +93,7 @@ def learner(room_code):
         # save the db to a file
         if app.debug: save_db()
 
+        # Show the page
         context = {
             'room_code' : room_code,
             'first_name' : first_name,
@@ -94,14 +104,80 @@ def learner(room_code):
     
     return render_template(template_name, room_code=room_code)
 
+
 @app.route('/<room_code>/tutor', methods=['GET', 'POST'])
-def tutor(room_code):
-    if request.method == 'POST':
-        name = request.form.get('name')
-        status = request.form.get('status')
-        return f"Room {room_code}: {name} - {status}"
+def tutor_view(room_code):
+    template_name = 'tutor.html'
     
-    return render_template('tutor.html', room_code=room_code)
+    # Make sure that the room code is valid
+    room_code = validate_room_code(room_code)
+
+    # Get url for learners
+    url = request.host_url.rstrip('/')+url_for('home')+room_code.lower()
+    
+    # Get learner records from the database for this room
+    learners = db.get(room_code, {'learners':{}})['learners']
+
+    # Show the page
+    context = {
+        'room_code': room_code,
+        'learners': learners,
+        'url': url,
+    }
+    return render_template(template_name, **context)
+
+
+@app.route('/<room_code>/poll', methods=['GET'])
+def poll_view(room_code):
+    template_name = 'poll.html'
+    room_code = validate_room_code(room_code)
+
+    # Get learner records from the database for this room
+    learners = db.get(room_code, {'learners':{}})['learners']
+
+    # Get learner records from the database for this room
+    learner_count = len(learners)
+
+    # Group by status
+    status_counts = {}
+    
+    for _,learner in learners.items():
+        if learner['status'] == 'clear': continue  # ignore status of clear
+        if learner['status'] not in status_counts:
+            status_counts[learner['status']] = []
+        status_counts[learner['status']].append(learner['first_name'])
+
+    # Get the maximum count of learners in any status
+    max_count = max((len(learners) for learners in status_counts.values()), default=0)
+
+    # Show the page
+    context = {
+        'room_code': room_code,
+        'learner_count': learner_count,
+        'status_counts': status_counts,
+        'max_count': max_count,
+    }
+    return render_template(template_name, **context)
+
+
+# Static Views
+@app.route('/about', methods=['GET'])
+def about_page():
+    template_name = 'about.html'
+    context = {
+    }
+    return render_template(template_name, **context)
+
+@app.route('/browser-check', methods=['GET'])
+def browser_check():
+    template_name = 'browser-check.html'
+    context = {
+        'message': 'Browser Check!'
+    }
+    return render_template(template_name, **context)
+
+
+
 
 
 if __name__ == '__main__':
